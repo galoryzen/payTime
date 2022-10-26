cdccda98aca6574b6553b65a46617046e8d03f52
Raul Lopez <rjlopezgr@gmail.com>
2022-10-26 16:15:45 -0500

    added user and bank routes

---
+++ backend/src/routes/bank.ts
@@ -0,0 +1,88 @@
+import { PrismaClient } from '@prisma/client';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+import { Type, Static } from '@sinclair/typebox'
+
+async function routes(fastify: FastifyInstance, options: any){
+    const server = fastify.withTypeProvider<TypeBoxTypeProvider>();
+
+    server.get('/banks', {
+        schema: {
+            tags: ['Bank'],
+            response: {
+                200: Type.Array(Type.Object({
+                    id: Type.Number(),
+                    name: Type.String(),
+                })),
+            }
+        }
+    }, async (request, reply) => {
+        const banks = await server.prisma.bank.findMany();
+        return banks;
+    });
+
+    server.get('/bank/:id', {
+        schema: {
+            tags: ['Bank'],
+            params: Type.Object({
+                id: Type.Number(),
+            }),
+            response: {
+                200: Type.Object({
+                    id: Type.Number(),
+                    name: Type.String(),
+                }),
+                404: Type.Object({
+                    message: Type.String(),
+                }),
+            }
+        }
+    }, async (request, reply) => {
+        const bank = await server.prisma.bank.findUnique({
+            where: {
+                id: Number(request.params.id),
+            },
+        });
+        
+        if (!bank) {
+            reply.code(404);
+            return { message: 'Bank not found' };
+        }
+        
+        return bank;
+    });
+
+    server.post('/bank', {
+        schema: {
+            summary: 'Create a new bank',
+            tags: ['Bank'],
+            body: Type.Object({
+                name: Type.String(),
+            }),
+            response: {
+                201: Type.Object({
+                    message: Type.String(),
+                }),
+                500: Type.Object({
+                    message: Type.String(),
+                }),
+            }
+        }
+    }, async (request, reply) => {
+        try {
+            await server.prisma.bank.create({
+                data: {
+                    name: request.body.name,
+                },
+            });
+            reply.code(201);
+            return { message: 'Bank created' };
+        } catch (error) {
+            reply.code(500);
+            console.log(error);
+            return { message: 'Error creating bank' };
+        }
+    });
+}
+
+module.exports = routes;

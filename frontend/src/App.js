11d11ff2ec074a223c4f9b0af203fc3b78ab604a
Luisa Escobar <lineroml@uninorte.edu.co>
2022-11-20 19:53:19 -0500

    Feat: Protected Routes + Login backend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Pay from './components/views/Pay';
 import Search from './components/views/Search';
 import PaymentMethod from './components/views/PaymentMethod';
 import PaymentMethodDetail from './components/views/PaymentMethodDetail';
+import ProtectedRoute from './components/authentication/ProtectedRoute';
 
 function App() {
   return (
@@ -13,11 +14,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/pay' element={<Pay />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/payment-methods' element={<PaymentMethod />} />
-        <Route path='/payment-methods/:payment_method_id' element={<PaymentMethodDetail />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/pay' element={<Pay />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/payment-methods' element={<PaymentMethod />} />
+          <Route path='/payment-methods/:payment_method_id' element={<PaymentMethodDetail />} />
+        </Route>
       </Routes>
     </div>
   );
--- frontend/src/components/authentication/Login.jsx
+++ frontend/src/components/authentication/Login.jsx
@@ -1,57 +1,91 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
-import loginImg from '../../assets/payTime.png'
+import loginImg from '../../assets/payTime.png';
+import useToken from '../../hooks/useToken';
 
 function Login() {
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const [token, setToken] = useState();
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const { token, setToken } = useToken();
 
-    const submitHandler = async e => {
-        e.preventDefault();
-        console.log(user, password);
-        axios.post('http://localhost:3000/login', {
-            email: user,
-            password: password
+  console.log('token', token);
+
+  if (token) {
+    return <Navigate to='/home' />;
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(user, password);
+    axios
+      .post(
+        'http://localhost:3000/login',
+        {
+          email: user,
+          password: password,
         },
         {
-            withCredentials: true
-        })
-        .then(res => {
-            console.log(res.data);
-            setToken(res.data.token);
-        })
-    }
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setToken(res.data);
+      });
+  };
 
-    return (
+  return (
     <div className='grid grid-cols-1 sm:grid-cols-2 h-screen'>
+      <div className='hidden sm:block'>
+        <img className='h-screen object-cover' src={loginImg} alt='logo' />
+      </div>
 
-        <div className='hidden sm:block'>
-            <img className='h-screen object-cover' src={loginImg} alt="logo"/>
-        </div>
-        
-        <div className='bg-white flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto p-8 px-8 rounded-lg shadow-lg' onSubmit={submitHandler}>
-                <h2 className='text-4xl dark:text-cyan-800 font-bold text-center shadow-slate-300'>Inicia Sesión</h2>
-                <div className='flex flex-col text-gray-700 py-2'>
-                    <label htmlFor="email">Email: </label>
-                    <input className='rounded-lg bg-white mt-2 p-2 shadow-lg shadow-slate-300 border-solid' type="user" name="user" value={user} onChange={(e)=>setUser(e.target.value)} id="user" />
-                </div>
-                <div className='flex flex-col text-gray-700 py-2'>
-                    <label htmlFor="password">Contraseña: </label>
-                    <input className='rounded-lg bg-white mt-2 p-2 shadow-lg shadow-slate-300 border-solid' type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)} id="password"/>
-                </div>
-                <Link to='/home' className='text-center text-orange-700 font-bold'>Login</Link>
-                <button className='w-full my-5 py-2 bg-amber-500 shadow-lg shadow-amber-500/50 hover:shadow-amber-500/40 text-white font-semibold rounded-lg'>Login</button>
-                <div>
-                    <Link to='/signup' className='text-center text-orange-700 font-bold'>¿No tienes cuenta? Regístrate</Link>
-                </div>
-            </form>
-        </div>
-
+      <div className='bg-white flex flex-col justify-center'>
+        <form
+          className='max-w-[400px] w-full mx-auto p-8 px-8 rounded-lg shadow-lg'
+          onSubmit={submitHandler}
+        >
+          <h2 className='text-4xl dark:text-cyan-800 font-bold text-center shadow-slate-300'>
+            Inicia Sesión
+          </h2>
+          <div className='flex flex-col text-gray-700 py-2'>
+            <label htmlFor='email'>Email: </label>
+            <input
+              className='rounded-lg bg-white mt-2 p-2 shadow-lg shadow-slate-300 border-solid'
+              type='user'
+              name='user'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              id='user'
+            />
+          </div>
+          <div className='flex flex-col text-gray-700 py-2'>
+            <label htmlFor='password'>Contraseña: </label>
+            <input
+              className='rounded-lg bg-white mt-2 p-2 shadow-lg shadow-slate-300 border-solid'
+              type='password'
+              name='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id='password'
+            />
+          </div>
+          <Link to='/home' className='text-center text-orange-700 font-bold'>
+            Login
+          </Link>
+          <button className='w-full my-5 py-2 bg-amber-500 shadow-lg shadow-amber-500/50 hover:shadow-amber-500/40 text-white font-semibold rounded-lg'>
+            Login
+          </button>
+          <div>
+            <Link to='/signup' className='text-center text-orange-700 font-bold'>
+              ¿No tienes cuenta? Regístrate
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login;

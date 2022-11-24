39182da14dace2cd8ba63551d76966a3d4f2c55b
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2022-11-24 13:22:37 -0500

    Show client cards in home page

--- frontend/src/components/views/Home.js
+++ frontend/src/components/views/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavBar from '../utils/NavBar.jsx';
 import User from '../utils/User.jsx';
-import CreditCard from '../utils/CreditCard.jsx';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
 import LastPayments from '../utils/LastPayments.jsx';
+import ReactLoading from 'react-loading';
 import useFetchUser from '../../hooks/useFetchUser.js';
+import useFetchCards from '../../hooks/useFetchCards';
 
 function Home() {
 
   var { user, loading} = useFetchUser();
+  var { cards: metodosPago, load } = useFetchCards();
 
   if(user !== undefined){
     localStorage.setItem('user', JSON.stringify(user));
@@ -15,8 +19,7 @@ function Home() {
   }
 
   if(user === undefined){
-    user = JSON.parse(localStorage.getItem('user'));
-    
+    user = JSON.parse(localStorage.getItem('user')); 
   }
 
   console.log(user);
@@ -25,16 +28,31 @@ function Home() {
     <div className='min-h-screen bg-sky-900'>
       <NavBar/>
         <div className=''>
-          <div className='flex justify-evenly	py-16'>
+          <div className='flex justify-center items-center py-16 gap-32'>
             <User nombre={user.name} email={user.email}/>
-            <CreditCard/>
+            {loading ? (
+            <div className='w-full h-[182.859px] flex justify-center items-center'>
+              <ReactLoading type='bubbles' color='white' height={100} width={100} />
+            </div>
+          ) : (
+            metodosPago.map((metodo, index) => (
+              <div className='flex flex-col items-end justify-center select-none '>
+                  <Cards
+                    preview
+                    cvc={metodo.cvv}
+                    name={metodo.name}
+                    number={metodo.cardNumber.replace(/\d{12}/g, '*'.repeat(12))}
+                    // expiry={metodo.mes + '/' + metodo.ano}
+                    issuer={metodo.provider.toLowerCase()}
+                  />
+              </div>
+            ))
+          )}
           </div>
-      
-        <div className=''>
-          <LastPayments/>
+          <div className=''>
+            <LastPayments/>
+          </div>
         </div>
-
-      </div>
     </div>
   );
 }
